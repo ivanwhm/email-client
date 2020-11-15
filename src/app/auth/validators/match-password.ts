986946b4d5535ba6105f9b1fd330725b1a856d71
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-15 18:25:34 -0300

    Connecting Custom Validators

--- src/app/auth/validators/match-password.ts
+++ src/app/auth/validators/match-password.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@angular/core';
 import { FormGroup, ValidationErrors, Validator } from '@angular/forms';
 
+@Injectable({ providedIn: 'root' })
 export class MatchPassword implements Validator {
   validate(formGroup: FormGroup): ValidationErrors {
     const { password, passwordConfirmation } = formGroup.value;

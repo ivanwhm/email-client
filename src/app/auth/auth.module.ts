60832b0d9501bad3f78744f289929f48d77f78fa
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-15 19:46:59 -0300

    Another Reusable Input

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,12 +3,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
-  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, HttpClientModule, SharedModule],
 })
 export class AuthModule {}
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
+  @Input() id: string;
+  @Input() label: string;
+  @Input() control: FormControl;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

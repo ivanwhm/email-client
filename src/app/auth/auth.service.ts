c35087142ab049d0f3972de860ca2597f77a82d2
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-29 18:06:05 -0300

    A Little Record Keeping

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+import { SignedInResponse } from './interfaces/signed-in-response.interface';
 import { SignupCredentials } from './interfaces/signup-credentials.interface';
 import { SignupResponse } from './interfaces/signup-response.interface';
 import { UsernameAvailableResponse } from './interfaces/username-available-response.interface';
@@ -16,13 +17,13 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  usernameAvailable(username: string) {
+  usernameAvailable(username: string): Observable<UsernameAvailableResponse> {
     return this.http.post<UsernameAvailableResponse>(`${this.rootUrl}/auth/username`, {
       username,
     });
   }
 
-  signup(credentials: SignupCredentials) {
+  signup(credentials: SignupCredentials): Observable<SignupResponse> {
     return this.http.post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials).pipe(
       tap(() => {
         this.signedin$.next(true);
@@ -30,10 +31,10 @@ export class AuthService {
     );
   }
 
-  checkAuth() {
-    return this.http.get(`${this.rootUrl}/auth/signedin`).pipe(
-      tap((response) => {
-        console.log(response);
+  checkAuth(): Observable<SignedInResponse> {
+    return this.http.get<SignedInResponse>(`${this.rootUrl}/auth/signedin`).pipe(
+      tap(({ authenticated }) => {
+        this.signedin$.next(authenticated);
       })
     );
   }

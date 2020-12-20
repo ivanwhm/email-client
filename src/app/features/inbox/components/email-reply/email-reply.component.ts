4d1b6b02b2733b3ce3523b9ed79907c4672271b3
Ivan Wilhelm <ivan.whm@icloud.com>
2020-12-20 19:08:18 -0300

    Formatting Reply Emails

--- src/app/features/inbox/components/email-reply/email-reply.component.ts
+++ src/app/features/inbox/components/email-reply/email-reply.component.ts
@@ -14,7 +14,17 @@ export class EmailReplyComponent implements OnInit {
 
   constructor(private readonly emailService: EmailService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const text = this.email.text.replace(/\n/gi, '\n> ');
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `Re: ${this.email.subject}`,
+      html: `\n\n\n-------- ${this.email.from} wrote:\n>${text}`,
+      text: `\n\n\n-------- ${this.email.from} wrote:\n>${text}`,
+    };
+  }
 
   onSubmit(email: Email): void {
     this.emailService.sendEmail(email).subscribe(() => {
